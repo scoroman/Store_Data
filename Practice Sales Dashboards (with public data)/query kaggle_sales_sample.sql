9580cc79aff396425e140d571fe5df14b5305669
scoroman <scoroman8@gmail.com>
2024-09-29 17:07:34 -0700

    sales dashboards

---
+++ Practice Sales Dashboards (with public data)/query kaggle_sales_sample.sql
@@ -0,0 +1,4 @@
+select * from sales_data_sample;
+select DISTINCT(YEAR_ID) From sales_data_sample;
+select count(ORDERNUMBER) from sales_data_sample;
+select MIN(PRICEEACH), MIN(QUANTITYORDERED), MAX(PRICEEACH), MAX(QUANTITYORDERED) From sales_data_sample; 
---
+++ Practice Sales Dashboards (with public data)/readme.txt
@@ -0,0 +1,2 @@
+The sql file db_dump_version_2.sql can be connected to the sales_after_feedback.pbix file to produce
+dynamic and interactive versions of the static dashboards you see in sales_dashboards.pdf
